6117f0cf269a328445e34de086e99788b8fe8b38
mario <mariolopezzbueno@gmail.com>
2024-11-19 15:22:55 +0100

    added external jars. updated image and manifest

--- tests/image/build.sh
+++ tests/image/build.sh
@@ -1,3 +1,3 @@
-podman build . --tag docker.io/kubernetesbigdataeg/nifi:1.27.0-1
+podman build . --tag docker.io/kubernetesbigdataeg/nifi:1.27.0-2
 podman login docker.io -u kubernetesbigdataeg
-podman push docker.io/kubernetesbigdataeg/nifi:1.27.0-1
+podman push docker.io/kubernetesbigdataeg/nifi:1.27.0-2
